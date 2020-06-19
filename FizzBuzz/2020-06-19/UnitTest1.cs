90fe350cc930bd78663178bae04d1c77c4effd96
Ilyana Smith <ilyana@kentsmiths.com>
2020-06-19 15:59:56 -0400

    pseudocode written

---
+++ FizzBuzz/2020-06-19/UnitTest1.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+
+//https://github.com/ardalis/kata-catalog/blob/master/katas/FizzBuzz.md
+
+// the FizzBuzz kata with the pseudocode programming process, then writing tests
+
+namespace _2020_06_19
+{
+    public class Formatter
+    {
+        public string FizzBuzz(int input) 
+        {
+            // declare string to hold input
+            // if input is divisible by 3 and 5, return "FizzBuzz"
+            // if input is divisible by 3 or 5
+                // if input is divisible by 3, return "Fizz"
+                // if input is divisible by 5, return "Buzz"
+            //else return input
+        }
+    }
+    
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
