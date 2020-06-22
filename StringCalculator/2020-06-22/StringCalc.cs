ec798cf2689ef9717c7019f60bb6a3367b0352c1
Ilyana Smith <ilyana@kentsmiths.com>
2020-06-22 14:23:09 -0400

    first test

---
+++ StringCalculator/2020-06-22/StringCalc.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace _2020_06_22
+{
+    public class StringCalc
+    {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
