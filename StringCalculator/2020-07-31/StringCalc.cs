47614528cd2184e4e69c6bc6df27ba650ebd1c32
Ilyana Smith <ilyana@kentsmiths.com>
2020-07-31 15:20:53 -0400

    starting point of 7-31

---
+++ StringCalculator/2020-07-31/StringCalc.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using Xunit;
+using Xunit.Sdk;
+
+namespace _2020_07_10
+{
+    public class StringCalc
+    {
+        public int Add(string nums)
+        {
+            return 0;
+
+        }
+    }
+}
---
+++ StringCalculator/2020-07-31/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace _2020_07_10
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void returns0GivenEmpty()
+        {
+            // Arrange 
+            String input = "";
+            var s = new StringCalc();
+
+            // Act
+            int output = s.Add(input);
+
+            // Assert
+            Assert.Equal(0, output);
+
+        }
+
+
+    }
+}
