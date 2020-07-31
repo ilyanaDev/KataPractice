e326a516deae2fb41655df16556fa6ba40cfed13
Ilyana Smith <ilyana@kentsmiths.com>
2020-07-31 15:26:17 -0400

    number 1 finished

--- StringCalculator/2020-07-31/StringCalc.cs
+++ StringCalculator/2020-07-31/StringCalc.cs
@@ -9,6 +9,26 @@ namespace _2020_07_10
     {
         public int Add(string nums)
         {
+            if(nums.Contains(","))
+            {
+                int sum = 0;
+
+                string[] numsArray = nums.Split(",");
+
+                foreach (string num in numsArray)
+                {
+                    sum += int.Parse(num);
+                }
+
+                return sum;
+
+            }
+
+            if(nums.Length == 1)
+            {
+                return int.Parse(nums);
+            }
+            
             return 0;
 
         }
--- StringCalculator/2020-07-31/UnitTest1.cs
+++ StringCalculator/2020-07-31/UnitTest1.cs
@@ -20,6 +20,36 @@ namespace _2020_07_10
 
         }
 
+        [Fact]
+        public void returnsNumGivenOneNum()
+        {
+            // Arrange 
+            String input = "5";
+            var s = new StringCalc();
+
+            // Act
+            int output = s.Add(input);
+
+            // Assert
+            Assert.Equal(5, output);
+
+        }
+
+        [Fact]
+        public void returnsSumGivenTwoNum()
+        {
+            // Arrange 
+            String input = "5,2";
+            var s = new StringCalc();
+
+            // Act
+            int output = s.Add(input);
+
+            // Assert
+            Assert.Equal(7, output);
+
+        }
+
 
     }
 }
