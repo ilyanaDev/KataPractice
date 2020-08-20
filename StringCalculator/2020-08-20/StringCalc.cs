df0c5b4154e5586df8bdbd64411308aba5bd4576
Ilyana Smith <ilyana@kentsmiths.com>
2020-08-20 14:49:42 -0400

    step 1 works

--- StringCalculator/2020-08-20/StringCalc.cs
+++ StringCalculator/2020-08-20/StringCalc.cs
@@ -9,7 +9,29 @@ namespace _2020_08_20
 {
     class StringCalc
     {
+        public int Add(string numbers)
+        {
+            int sum = 0;
 
+            if(numbers.Contains(","))
+            {
+                string[] numberArray = numbers.Split(",");
+
+                foreach(string n in numberArray)
+                {
+                    sum += int.Parse(n);
+                }
+
+                return sum;
+            }
+            
+            if(!(numbers.Contains(",")) && numbers.Length > 0)
+            {
+                return int.Parse(numbers);
+            }
+            
+            return 0;
+        }
 
     }
 }
--- StringCalculator/2020-08-20/UnitTest1.cs
+++ StringCalculator/2020-08-20/UnitTest1.cs
@@ -6,8 +6,47 @@ namespace _2020_08_20
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Returns0GivenEmpty()
         {
+            //Arrange
+            string input = "";
+            StringCalc sc = new StringCalc();
+
+            //Act
+            int output = sc.Add(input);
+
+            //Assert
+            Assert.Equal(0, output);
+
+        }
+
+        [Fact]
+        public void ReturnsNumGivenOneNum()
+        {
+            //Arrange
+            string input = "5";
+            StringCalc sc = new StringCalc();
+
+            //Act
+            int output = sc.Add(input);
+
+            //Assert
+            Assert.Equal(5, output);
+
+        }
+
+        [Fact]
+        public void ReturnsSumGivenTwoNum()
+        {
+            //Arrange
+            string input = "5,3";
+            StringCalc sc = new StringCalc();
+
+            //Act
+            int output = sc.Add(input);
+
+            //Assert
+            Assert.Equal(8, output);
 
         }
     }
