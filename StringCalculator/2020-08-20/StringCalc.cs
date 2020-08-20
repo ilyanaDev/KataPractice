d7f03b64954f9b7bc385aea0a5e8b6e91920f383
Ilyana Smith <ilyana@kentsmiths.com>
2020-08-20 14:42:52 -0400

    Starting Point

---
+++ StringCalculator/2020-08-20/StringCalc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+using Xunit.Sdk;
+
+namespace _2020_08_20
+{
+    class StringCalc
+    {
+
+
+    }
+}
